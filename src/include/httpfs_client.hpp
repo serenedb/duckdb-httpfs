65e3e96d334049c0922a84033c28d436a77c3ecd
pavel vedernikoff <pavel.ivanov@serenedb.com>
2026-08-31 14:47:20 +0200

    Merge extra headers idempotently instead of pre-merging them into a member

--- src/include/httpfs_client.hpp
+++ src/include/httpfs_client.hpp
@@ -48,7 +48,6 @@ struct HTTPFSParams : public HTTPParams {
 	bool s3_version_id_pinning {false};
 	shared_ptr<HTTPState> state;
 	string user_agent = {""};
-	bool pre_merged_headers = false;
 	idx_t force_download_threshold = 0;
 
 	// Additional fields needs to be appended at the end and need to be propagated to duckdb-wasm
